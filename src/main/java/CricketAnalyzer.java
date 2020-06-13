df5d33be96d7cb29cd2c2bed55959c8c2bfe7654
sanchez1023 <shubham1023@gmail.com>
2020-06-13 22:58:06 +0530

    ADDED CricketAnalyzerException class  and code to handle exception while loading Csv File data

--- src/main/java/CricketAnalyzer.java
+++ src/main/java/CricketAnalyzer.java
@@ -6,17 +6,20 @@ import java.util.List;
 
 public class CricketAnalyzer {
 
-    public int loadBattingData(String CsvFilePath) throws IOException, CSVBuilderException {
+    public int loadBattingData(String CsvFilePath) throws IOException, CSVBuilderException, CricketAnalyzerException {
 
-
+        try {
             Reader reader = Files.newBufferedReader(Paths.get(CsvFilePath));
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             List<IPLBattingCSV> battingCSVList = csvBuilder.getCSVFileList(reader, IPLBattingCSV.class);
             System.out.println("value of list" + battingCSVList);
             return battingCSVList.size();
 
-
-
+        } catch (IOException e) {
+        throw new CricketAnalyzerException(e.getMessage(),CricketAnalyzerException.ExceptionType.BATTING_CSV_FILE_PATH);
+        } catch (CSVBuilderException e) {
+    throw new CricketAnalyzerException(e.getMessage(),e.type.name());
+        }
 
 
     }
--- src/test/java/LeagueAnalyzerTest.java
+++ src/test/java/LeagueAnalyzerTest.java
@@ -7,12 +7,20 @@ public class LeagueAnalyzerTest {
 
     private static final String BATTING_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     @Test
-    public void givenBattingCsvFile_ShouldReturnCsvFileData() throws IOException, CSVBuilderException {
+    public void givenBattingCsvFile_ShouldReturnCsvFileData() throws IOException ,CSVBuilderException  {
+        try {
 
             CricketAnalyzer cricketAnalyzer = new CricketAnalyzer();
             int numberOfRecords = cricketAnalyzer.loadBattingData(BATTING_CSV_FILE_PATH);
             Assert.assertEquals(101, numberOfRecords);
+        }
+        catch (CricketAnalyzerException e){
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CSVBuilderException e) {
+            e.printStackTrace();
+        }
 
     }
 
